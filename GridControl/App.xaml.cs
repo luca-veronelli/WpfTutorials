e2e11c49975371ebd09293851b962bdae0415d95
Luca VERONELLI <luca.veronelli@alten.it>
2024-05-14 17:17:49 +0200

    GridControl created

---
+++ GridControl/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace GridControl
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
