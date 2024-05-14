a7dc58d079ce09ef02cbc6a8ac8e65770086f511
Luca VERONELLI <luca.veronelli@alten.it>
2024-05-14 17:02:38 +0200

    WpfTutorial2 created

---
+++ WpfTutorial2/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace WpfTutorial2
+{
+    public partial class MainWindow : Window
+    {
+        bool isRunning = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnToggleRun_Click(object sender, RoutedEventArgs e)
+        {
+            if (isRunning)
+            {
+                tbStatus.Text = "Stopped, click to run";
+                btnToggleRun.Content = "Run";
+            }
+            else
+            {
+                tbStatus.Text = "Running, click to stop";
+                btnToggleRun.Content = "Stop";
+            }
+
+            isRunning = !isRunning;
+        }
+    }
+}
