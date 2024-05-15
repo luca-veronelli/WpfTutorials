69a7b691b8e456a3a23caec8f093cfe1226f840d
Luca VERONELLI <luca.veronelli@alten.it>
2024-05-15 12:00:09 +0200

    CustomTextBoxControl created

---
+++ CustomTextBoxControl/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace CustomTextBoxControl
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CustomTextBoxControl/View/UserControls/ClearableTextBox.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CustomTextBoxControl.View.UserControls
+{
+    public partial class ClearableTextBox : UserControl
+    {
+        public ClearableTextBox()
+        {
+            InitializeComponent();
+        }
+
+        private string? placeholder;
+
+        public string? Placeholder
+        {
+            get { return placeholder; }
+
+            // this should be done different using OnPropertyChanged()
+            set 
+            { 
+                placeholder = value;
+                tbPlaceholder.Text = placeholder;
+            }
+        }
+
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtInput.Clear();
+            txtInput.Focus();
+        }
+
+        private void txtInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtInput.Text))
+            {
+                tbPlaceholder.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                tbPlaceholder.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+}
