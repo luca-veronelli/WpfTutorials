7a1364037186cfc7a20cc6561ab405b0246ab707
Luca VERONELLI <luca.veronelli@alten.it>
2024-05-15 14:59:38 +0200

    MessageBoxTutorial created

---
+++ MessageBoxTutorial/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace MessageBoxTutorial
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFire_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("Could not open file", "ERROR!", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            MessageBoxResult result = MessageBox.Show("Do you agree?", "Agreement", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                tbInfo.Text = "Agreed";
+            }
+            else
+            {
+                tbInfo.Text = "Not agreed";
+            }
+        }
+    }
+}
