c222f1f6ad91be671d158a40f3b6a303ce593277
Luca VERONELLI <luca.veronelli@alten.it>
2024-05-15 15:54:47 +0200

    OpenFileDialogTutorial created

---
+++ OpenFileDialogTutorial/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+
+namespace OpenFileDialogTutorial
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnFire_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "C# source files | *.cs";
+            //fileDialog.InitialDirectory = "C:\\temp";
+            fileDialog.Title = "Please, pick a C# source a file(s)";
+            fileDialog.Multiselect = true;
+
+            bool? success = fileDialog.ShowDialog();
+            if (success == true)
+            {
+                string[] path = fileDialog.FileNames;
+                string[] fileName = fileDialog.SafeFileNames;
+
+                //tbInfo.Text = path;
+            }
+            else
+            {
+                // TODO
+            }
+        }
+    }
+}
